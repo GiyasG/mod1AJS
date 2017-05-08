74809897830f6e9984adbf545ea5560272558083
User1 <C:\Users\Embassy\AppData\Roaming\The Bat!>
2017-05-08 09:43:59 +0300

    Final version of modulu1 test

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,41 @@
 (function () {
 'use strict';
 
-angular.module('DIApp', [])
-.controller('DIController', DIController);
+angular.module('LunchCheck', [])
+.controller('LunchCheckerController', LunchCheckerController);
 
-function DIController ($scope,
-                       $filter,
-                       $injector) {
-  $scope.name = "Yaakov";
+LunchCheckerController.$inject = ['$scope', '$filter'];
+function LunchCheckerController($scope, $filter) {
+  $scope.inputMsg = "";
+  $scope.statusMsg = "";
+  $scope.csscolor = {};
+  $scope.csscolorbox = {};
 
-  $scope.upper = function () {
-    var upCase = $filter('uppercase');
-    $scope.name = upCase($scope.name);
+  $scope.checkMsg = function () {
+    var InMsg = $scope.inputMsg;
+    var arr = InMsg.split(",");
+
+    // console.log(ccolor);
+    // console.log(StMsg);
+    // console.log(arr.length);
+
+    if (arr == 0) {
+      $scope.statusMsg = "Please enter data first";
+      $scope.csscolor = {"color" : "red"};
+      $scope.csscolorbox = {"border-color" : "red"};
+    }
+    else if (arr.length <= 3) {
+      $scope.statusMsg = "Enjoy";
+      $scope.csscolor = {"color" : "green"} ;
+      $scope.csscolorbox = {"border-color" : "green"};
+    }
+    else {
+      $scope.statusMsg = "Too much";
+      $scope.csscolor = {"color" : "green"} ;
+      $scope.csscolorbox = {"border-color" : "green"};
+    }
+
   };
-
-  console.log($injector.annotate(DIController));
 }
-
-function AnnonateMe(name, job, blah) {
-  return "Blah!";
-}
-
-console.log(DIController.toString());
 
 })();
